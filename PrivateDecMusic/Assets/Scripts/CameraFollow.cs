6f5c4e2e659ca51869c843bb67b3d30189cd98a5
SinnabarDev <killer580death@yahoo.com>
2026-04-21 18:25:21 -0700

    animation and partrol logic
    added MC animation and fixed the inrange logic to include hiding.

--- PrivateDecMusic/Assets/Scripts/CameraFollow.cs
+++ PrivateDecMusic/Assets/Scripts/CameraFollow.cs
@@ -9,7 +9,19 @@ public class CameraFollow : MonoBehaviour
     void LateUpdate()
     {
         Vector3 desiredPosition = player.position + offset;
+                if(Mathf.Abs(desiredPosition.x) > 1.5f )
+        {
+            if (desiredPosition.x > 0)
+            {
+                desiredPosition.x = 1.5f;
+            }
+            else if (desiredPosition.x < 0)
+            {
+                desiredPosition.x = -1.5f;
+            }
+        }
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
         transform.position = new Vector3(smoothedPosition.x, smoothedPosition.y, transform.position.z);
     }
 }
