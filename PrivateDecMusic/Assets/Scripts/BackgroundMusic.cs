2754333c6f649f35fcbbb6ea28613c0e6a61a4f6
BilalPayton <supersmashorange@gmail.com>
2026-05-31 01:42:08 -0700

    Added more music to Scenes 1, 2, & 3

--- PrivateDecMusic/Assets/Scripts/BackgroundMusic.cs
+++ PrivateDecMusic/Assets/Scripts/BackgroundMusic.cs
@@ -4,13 +4,20 @@ public class BackgroundMusic : MonoBehaviour
 { 
     
     [SerializeField] private GameObject _rhythmUI;
+    [SerializeField] private Animator _npcState;
     [SerializeField] private AudioSource _backgroundMusic;
+    [SerializeField] private AudioClip _beforeMusic;
+    [SerializeField] private AudioClip _afterMusic;
 
     private bool _playMusic;
+    private int _checkHypno; // 0 = no, 1 = yes
+
 
     void Start()
     {
+        _backgroundMusic.clip = _beforeMusic;
         _playMusic = true;
+        _checkHypno = 0;
     }
 
 
@@ -18,6 +25,7 @@ public class BackgroundMusic : MonoBehaviour
     {
         if (_rhythmUI.activeInHierarchy == false && _playMusic == true)
         {
+            
             _backgroundMusic.Play();
             _playMusic = false;
         }
@@ -25,6 +33,14 @@ public class BackgroundMusic : MonoBehaviour
         else if (_rhythmUI.activeInHierarchy == true)
         {
             _backgroundMusic.Stop();
+            _playMusic = true;
+        }
+        
+        if (_npcState.GetBool("isHypno") == false && _checkHypno == 0)
+        {
+            _backgroundMusic.clip = _afterMusic;
+            _backgroundMusic.Play();
+            _checkHypno++;
         }
 
 
